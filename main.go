c1c7f082cb97a98efa30ef30c06ba9cd486046b5
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2024-06-09 17:15:28 +0000

    feat: Add JSON output to main.go

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"math/rand/v2"
 )
 
+type Output struct {
+	RandomNumber int `json:"randomNumber"`
+}
+
 func main() {
 	// Seed the random number generator with a random number
-	println(rand.IntN(32))
+	n := rand.IntN(32)
+
+	// Create a new Output struct
+	output := Output{
+		RandomNumber: n,
+	}
+
+	// Create json
+	jsonOutput, err := json.Marshal(&output)
+	if err != nil {
+		println("Error marshalling to JSON:", err)
+		return
+	}
+
+	// Print the json
+	println(string(jsonOutput))
 }
